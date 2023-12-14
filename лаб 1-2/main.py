a56df840ff9c875eb6c66cf80ce903e47bc5e50d
Refs14 <126952245+Refs14@users.noreply.github.com>
2023-12-14 23:43:09 +0300

    Update main.py

--- лаб 1-2/main.py
+++ лаб 1-2/main.py
@@ -1,8 +1,55 @@
+import sqlite3
 import random
 
-a = 1000*random.random()
-l = []
-for i in range(1, 10):
-    l.append((i, round(a*random.uniform(0.8, 1.1))+9.99, i))
+conn = sqlite3.connect('misis.db')
 
-print(l)
+cursor = conn.cursor()
+
+# создаем таблицу c ценами
+cursor.execute('''CREATE TABLE localization
+            	    (localization_id INTEGER PRIMARY KEY,
+                    country TEXT,
+                    currency TEXT,
+                    currency_weight DOUBLE)''')
+
+locals = [(1,'Россия', 'рубль', 1.0),
+        (2,'Китай', 'юань', 12.66),
+        (3,'Турция', 'лира', 3.09),
+        (4,'Аргентина', 'песо', 0.24),
+        (5,'Казахстан', 'тенге', 0.20),
+        (6,'Германия', 'евро', 98.38),
+        (7,'США', 'доллар США', 89.48),
+        (8,'Япония', 'йена', 0.63),
+        (9,'Швейцария', 'швейцарский франк', 103.29),
+        (10,'Австралия', 'австралийский доллар', 59.95)]
+
+# добавляем в таблицу localization 10 записей выше
+#cursor.executemany('INSERT INTO localization VALUES (?, ?, ?, ?)', locals)
+
+
+# создаем таблицу c локализациями
+cursor.execute('''CREATE TABLE price
+            	    (price_id INTEGER PRIMARY KEY, 
+                    game TEXT, price DOUBLE, 
+                    localization_id INTEGER
+                    FOREIGN KEY(localization_id) REFERENCES localization(localization_id))''')
+
+#дубль цен из переменной locals
+cur_weight = [1.0, 12.66, 3.09, 0.24, 0.20, 98.38, 89.48, 0.63, 103.29, 59.95]
+
+#создаем list из 100 названий игр
+file = open("games.txt")
+file_str = file.read()
+games = file_str.split('\n')
+
+prices = []
+
+for i in range(100):
+    current_geme_price = random.uniform(300.0, 5000.0)
+    for j in range(1, 11):
+        prices.append((i*10+j, games[i], current_geme_price*random.uniform(0.8, 1.2)/cur_weight[j-1], j))
+
+conn.commit()
+conn.close()
+
+cursor.fetchall()
