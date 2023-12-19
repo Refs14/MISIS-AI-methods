94ba7ab01b01760fdd15bc03bf6937f72f78b189
Refs14 <126952245+Refs14@users.noreply.github.com>
2023-12-20 00:49:47 +0300

    bot v1.4

--- лаб 1-2/actions/actions.py
+++ лаб 1-2/actions/actions.py
@@ -6,6 +6,8 @@ from datetime import datetime as dt
 from typing import Any, Text, Dict, List
 import sqlite3
 import random
+from difflib import SequenceMatcher
+import io
  
 # поиск 3 локаций с наименьшими ценами для введенной игры
 class ActionGameMinPrices(Action):
@@ -15,11 +17,14 @@ class ActionGameMinPrices(Action):
  
     def run(self, dispatcher:CollectingDispatcher, tracker:Tracker, domain:Dict[Text,Any]) -> List[Dict[Text, Any]]:
  
-        info_game = tracker.get_slot("GAME")
-
+        game_list = tracker.get_slot("GAME_LIST")
+        index = int(tracker.get_slot("GAME_NUMBER"))
+        if(index > 10 or index < 1):
+            dispatcher.utter_message(text=f'Введенное число должно быть от 1 до 10')
+            return []
         conn = sqlite3.connect('misis.db')
         cursor = conn.cursor()
-        cursor.execute(f"SELECT g.name, round(p.price*l.currency_weight, 2) as rouble_price, l.country from price as p, localization as l, game as g where l.localization_id=p.localization_id and g.game_id=p.game_id and g.name = 'Contra' order by rouble_price limit 3")
+        cursor.execute(f"SELECT g.name, round(p.price*l.currency_weight, 2) as rouble_price, l.country from price as p, localization as l, game as g where l.localization_id=p.localization_id and g.game_id=p.game_id and g.name = '{game_list[index-1][0]}' order by rouble_price limit 3")
         data_raw = cursor.fetchall()
         if(len(data_raw)==0):
             dispatcher.utter_message(text=f'Игра не найдена: ее нет в базе данных или название введено неверно')
@@ -30,6 +35,7 @@ class ActionGameMinPrices(Action):
                 dispatcher.utter_message(text=f'Цена: {data[1]} рубля, страна: {data[2]}\n')
 
         return []
+
 # предложить рандомную игру и 3 самые низкие цены
 class ActionRandomGame(Action):
  
@@ -87,9 +93,15 @@ class ActionRandomGameInGenre(Action):
     def run(self, dispatcher:CollectingDispatcher, tracker:Tracker, domain:Dict[Text,Any]) -> List[Dict[Text, Any]]:
  
         genre_number = tracker.get_slot("GENRE_NUMBER")
-        test = 2
-        dispatcher.utter_message(text=f'GENRE_NUMBER: {genre_number}')
-
+        try:
+            string_int = int(genre_number)
+        except ValueError:
+            dispatcher.utter_message(text=f'Ошибка: введенный текст не соответствует числу от 1 до 10')
+            return []
+        int_genre = int(genre_number)
+        if(int_genre > 10 or int_genre < 1):
+            dispatcher.utter_message(text=f'Ошибка: введенный текст не соответствует числу от 1 до 10')
+            return []
         conn = sqlite3.connect('misis.db')
         cursor = conn.cursor()
 
@@ -104,17 +116,19 @@ class ActionRandomGameInGenre(Action):
                         and
                         gen.genre_id = gg.genre_id
                         and
-                        gen.genre_id = {test}
+                        gen.genre_id = {int_genre}
                 group by g.name
                 order by rouble_price''')
-        #data_raw = cursor.fetchall()
+        data_raw = cursor.fetchall()
 
-        #data = list(data_raw[random.randint(1, len(data_raw))])
-        #dispatcher.utter_message(text=f'Игра {data[0]} стоит {data[1]} рубля в {data[2]}')
+        data = list(data_raw[random.randint(1, len(data_raw))])
+        dispatcher.utter_message(text=f'Выбран жанр: {data[3]}')
+        dispatcher.utter_message(text=f'Игра {data[0]} стоит {data[1]} рубля в {data[2]}')
 
         conn.close()
         return []
 
+#выбор одного из 4 сценариев
 class ActionChooseOption(Action):
  
     def name(self) -> Text:              # регистрируем имя действия
@@ -130,8 +144,33 @@ class ActionChooseOption(Action):
                 {"payload": "/random_game_in_genre", "title": "Найти рандомную игру из жанра"}
             ])
         return []
-    
 
+# считать строку и найти наиболее похожее название игры
+class ActionGetGame(Action):
+    def name(self):
+        return "action_get_game"
+
+    def run(self, dispatcher, tracker, domain):
+
+        string = str(tracker.get_slot("GAME"))
+        dispatcher.utter_message(text=f'Ищем: {string}')
+
+        file = io.open("games.txt", mode="r", encoding="utf-8")
+        game_list = file.read().split('\n')
+        match_list = []
+        for game in game_list:
+            match = SequenceMatcher(None, string.lower(), game.lower()).find_longest_match(0, len(string), 0, len(game))
+            match_list.append([game, match.size])
+
+        match_list = sorted(match_list, key=lambda x: -x[1])
+        if(len(match_list) > 10):
+            match_list = match_list[0:10]
+        dispatcher.utter_message(text=f'Найдены игры:')
+        for i in range(len(match_list)):
+            dispatcher.utter_message(text=f'{i+1}. {match_list[i][0]} (метрика {match_list[i][1]})')
+        return [SlotSet("GAME_LIST", match_list)]
+
+# предложение выбрать жанр
 class ActionGetGenre(Action):
     def name(self):
         return "action_get_genre"
