e69a86f7f0d71e30347b8136b23f22c771aba9fd
Refs14 <126952245+Refs14@users.noreply.github.com>
2023-12-19 18:33:35 +0300

    Add files via upload

--- лаб 1-2/actions/actions.py
+++ лаб 1-2/actions/actions.py
@@ -7,10 +7,11 @@ from typing import Any, Text, Dict, List
 import sqlite3
 import random
  
-class ActionMinPriceForGame(Action):
+# поиск 3 локаций с наименьшими ценами для введенной игры
+class ActionGameMinPrices(Action):
  
     def name(self) -> Text:              # регистрируем имя действия
-        return "action_find_prices"
+        return "action_game_min_prices"
  
     def run(self, dispatcher:CollectingDispatcher, tracker:Tracker, domain:Dict[Text,Any]) -> List[Dict[Text, Any]]:
  
@@ -18,10 +19,10 @@ class ActionMinPriceForGame(Action):
 
         conn = sqlite3.connect('misis.db')
         cursor = conn.cursor()
-        cursor.execute(f"SELECT p.game, round(p.price*l.currency_weight, 2) as rouble_price, l.country from price as p, localization as l where l.localization_id=p.localization_id and p.game = '{info_game}' order by rouble_price limit 3")
+        cursor.execute(f"SELECT g.name, round(p.price*l.currency_weight, 2) as rouble_price, l.country from price as p, localization as l, game as g where l.localization_id=p.localization_id and g.game_id=p.game_id and g.name = 'Contra' order by rouble_price limit 3")
         data_raw = cursor.fetchall()
         if(len(data_raw)==0):
-            dispatcher.utter_message(text=f'Игра {info_game} не найдена: ее нет в базе данных или название введено неверно')
+            dispatcher.utter_message(text=f'Игра не найдена: ее нет в базе данных или название введено неверно')
         else:
             dispatcher.utter_message(text=f'Вот топ-3 низких цен и их стран за игру {data_raw[0][0]}\n')
             for el in data_raw:
@@ -29,11 +30,11 @@ class ActionMinPriceForGame(Action):
                 dispatcher.utter_message(text=f'Цена: {data[1]} рубля, страна: {data[2]}\n')
 
         return []
-    
-class ActionOfferRandomGame(Action):
+# предложить рандомную игру и 3 самые низкие цены
+class ActionRandomGame(Action):
  
     def name(self) -> Text:              # регистрируем имя действия
-        return "action_offer_random"
+        return "action_random_game"
  
     def run(self, dispatcher:CollectingDispatcher, tracker:Tracker, domain:Dict[Text,Any]) -> List[Dict[Text, Any]]:
  
@@ -41,10 +42,112 @@ class ActionOfferRandomGame(Action):
 
         conn = sqlite3.connect('misis.db')
         cursor = conn.cursor()
-        cursor.execute(f"SELECT p.game, round(p.price*l.currency_weight, 2) as rouble_price, l.country from price as p, localization as l where l.localization_id=p.localization_id and p.price_id = '{index}' order by rouble_price limit 1")
+        index = random.randint(1, 100)
+        cursor.execute(f"SELECT g.name, round(p.price*l.currency_weight, 2) as rouble_price, l.country from price as p, localization as l, game as g where l.localization_id=p.localization_id and g.game_id=p.game_id and g.game_id = {index} order by rouble_price limit 3")
         data_raw = cursor.fetchall()
 
-        data = list(data_raw[0])
-        dispatcher.utter_message(text=f'Игра {data[0]} стоит {data[1]} рубля в {data[2]}')
+        data = [list(row) for row in data_raw]
+        dispatcher.utter_message(text=f'Вот топ 3 низких цен для игры {data[0][0]}:')
+        for i in range(3):
+            dispatcher.utter_message(text=f'Игра стоит {data[i][1]} рубля в {data[i][2]}')
 
+        return []
+    
+# поиск самых дешевых игр (без разделения на локации)
+class ActionCheapestGame(Action):
+ 
+    def name(self) -> Text:              # регистрируем имя действия
+        return "action_cheapest_game"
+ 
+    def run(self, dispatcher:CollectingDispatcher, tracker:Tracker, domain:Dict[Text,Any]) -> List[Dict[Text, Any]]:
+
+        conn = sqlite3.connect('misis.db')
+        cursor = conn.cursor()
+        cursor.execute(f'''SELECT g.name, round(MIN(p.price*l.currency_weight), 2) as rouble_price, l.country 
+                from price as p, localization as l, game as g 
+                where 
+                        l.localization_id=p.localization_id 
+                        and
+                        g.game_id=p.game_id 
+                group by g.name
+                order by rouble_price limit 3''')
+        data_raw = cursor.fetchall()
+
+        for i in range(3):
+            dispatcher.utter_message(text=f'Игра {data_raw[i][0]} стоит {data_raw[i][1]} рубля в {data_raw[i][2]}')
+
+        return []
+    
+# поиск рандомной игры определенного жанра по мминимальной цене
+class ActionRandomGameInGenre(Action):
+ 
+    def name(self) -> Text:              # регистрируем имя действия
+        return "action_random_game_in_genre"
+ 
+    def run(self, dispatcher:CollectingDispatcher, tracker:Tracker, domain:Dict[Text,Any]) -> List[Dict[Text, Any]]:
+ 
+        genre_number = tracker.get_slot("GENRE_NUMBER")
+        test = 2
+        dispatcher.utter_message(text=f'GENRE_NUMBER: {genre_number}')
+
+        conn = sqlite3.connect('misis.db')
+        cursor = conn.cursor()
+
+        cursor.execute(f'''SELECT g.name, round(MIN(p.price*l.currency_weight), 2) as rouble_price, l.country, gen.name
+                from price as p, localization as l, game as g, genre as gen, game_genre as gg
+                where 
+                        l.localization_id=p.localization_id 
+                        and
+                        g.game_id=p.game_id
+                        and
+                        gg.game_id = g.game_id
+                        and
+                        gen.genre_id = gg.genre_id
+                        and
+                        gen.genre_id = {test}
+                group by g.name
+                order by rouble_price''')
+        #data_raw = cursor.fetchall()
+
+        #data = list(data_raw[random.randint(1, len(data_raw))])
+        #dispatcher.utter_message(text=f'Игра {data[0]} стоит {data[1]} рубля в {data[2]}')
+
+        conn.close()
+        return []
+
+class ActionChooseOption(Action):
+ 
+    def name(self) -> Text:              # регистрируем имя действия
+        return "action_choose_option"
+ 
+    def run(self, dispatcher: CollectingDispatcher,
+            tracker: Tracker,
+            domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
+        dispatcher.utter_message(buttons = [
+                {"payload": "/game_min_prices", "title": "Найти топ 3 низких цен для игры"},
+                {"payload": "/random_game", "title": "Найти топ 3 низких цен для случайной игры"},
+                {"payload": "/cheapest_game", "title": "Найти топ 3 самые дешевые игры"},
+                {"payload": "/random_game_in_genre", "title": "Найти рандомную игру из жанра"}
+            ])
+        return []
+    
+
+class ActionGetGenre(Action):
+    def name(self):
+        return "action_get_genre"
+
+    def run(self, dispatcher, tracker, domain):
+        genres = [(0,'казуальная'),
+        (1,'экшен'),
+        (2,'шутер'),
+        (3,'RPG'),
+        (4,'стратегия'),
+        (5,'хоррор'),
+        (6,'стелс'),
+        (7,'приключение'),
+        (8,'файтинг'),
+        (9,'выживание')]
+        dispatcher.utter_message(text="Выберите номер жанра:")
+        for i in range(10):
+            dispatcher.utter_message(text=f'{i+1}. {genres[i][1]}')
         return []
--- лаб 1-2/main.py
+++ лаб 1-2/main.py
@@ -5,12 +5,39 @@ conn = sqlite3.connect('misis.db')
 
 cursor = conn.cursor()
 
-# создаем таблицу c ценами
-cursor.execute('''CREATE TABLE localization
-            	    (localization_id INTEGER PRIMARY KEY,
+# создаем таблицу c локализациями
+cursor.execute('''CREATE TABLE IF NOT EXISTS localization
+                 (localization_id INTEGER PRIMARY KEY,
                     country TEXT,
                     currency TEXT,
                     currency_weight DOUBLE)''')
+
+# создаем таблицу c играми
+cursor.execute('''CREATE TABLE IF NOT EXISTS game
+                 (game_id INTEGER PRIMARY KEY, 
+                    name TEXT)''')
+
+# создаем таблицу c ценами
+cursor.execute('''CREATE TABLE IF NOT EXISTS price
+                 (price_id INTEGER PRIMARY KEY, 
+                    price DOUBLE, 
+                    game_id INTEGER, 
+                    localization_id INTEGER,
+                    FOREIGN KEY(game_id) REFERENCES game(game_id),
+                    FOREIGN KEY(localization_id) REFERENCES localization(localization_id))''')
+
+# создаем таблицу c жанрами
+cursor.execute('''CREATE TABLE IF NOT EXISTS genre
+                 (genre_id INTEGER PRIMARY KEY, 
+                    name TEXT)''')
+
+# создаем таблицу для связи NxN между играми и жанрами
+cursor.execute('''CREATE TABLE IF NOT EXISTS game_genre
+                 (game_id INTEGER, 
+                    genre_id INTEGER, 
+                    FOREIGN KEY(game_id) REFERENCES game(game_id), 
+                    FOREIGN KEY(genre_id) REFERENCES genre(genre_id))''')
+
 
 locals = [(1,'Россия', 'рубль', 1.0),
         (2,'Китай', 'юань', 12.66),
@@ -26,15 +53,6 @@ locals = [(1,'Россия', 'рубль', 1.0),
 # добавляем в таблицу localization 10 записей выше
 cursor.executemany('INSERT INTO localization VALUES (?, ?, ?, ?)', locals)
 
-
-# создаем таблицу c локализациями
-cursor.execute('''CREATE TABLE price
-            	    (price_id INTEGER PRIMARY KEY, 
-                    game TEXT, 
-                    price DOUBLE, 
-                    localization_id INTEGER,
-                    FOREIGN KEY(localization_id) REFERENCES localization(localization_id))''')
-
 #дубль цен из переменной locals
 cur_weight = [1.0, 12.66, 3.09, 0.24, 0.20, 98.38, 89.48, 0.63, 103.29, 59.95]
 
@@ -42,15 +60,40 @@ cur_weight = [1.0, 12.66, 3.09, 0.24, 0.20, 98.38, 89.48, 0.63, 103.29, 59.95]
 file = open("games.txt")
 file_str = file.read()
 games = file_str.split('\n')
+game_and_index = [[i+1, games[i]] for i in range(100)]
+
+cursor.executemany('INSERT INTO game VALUES (?, ?)', game_and_index)
 
 prices = []
 
 for i in range(100):
     current_geme_price = random.uniform(300.0, 5000.0)
     for j in range(1, 11):
-        prices.append((i*10+j, games[i], current_geme_price*random.uniform(0.8, 1.2)/cur_weight[j-1], j))
+        prices.append((i*10+j, current_geme_price*random.uniform(0.8, 1.2)/cur_weight[j-1], i+1, j))
 
 cursor.executemany('INSERT INTO price VALUES (?, ?, ?, ?)', prices)
+
+genres = [(1,'казуальная'),
+        (2,'экшен'),
+        (3,'шутер'),
+        (4,'RPG'),
+        (5,'стратегия'),
+        (6,'хоррор'),
+        (7,'стелс'),
+        (8,'приключение'),
+        (9,'файтинг'),
+        (10,'выживание'),]
+
+cursor.executemany('INSERT INTO genre VALUES (?, ?)', genres)
+
+game_genres = []
+autoinc = 1
+for i in range(1, 101):
+    for j in range(1, 11):
+        if(random.random() < 0.3):
+            game_genres.append([i, j])
+
+cursor.executemany('INSERT INTO game_genre VALUES (?, ?)', game_genres)
 
 conn.commit()
 conn.close()
